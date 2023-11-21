bc1b991cf77750a3b05c361f1c15b310e05ec5a0
nathansamuell <nathansamuell@duck.com>
2023-11-20 23:21:57 -0600

    fixed typo lol

--- installessentials.sh
+++ installessentials.sh
@@ -3,7 +3,7 @@
 install_from_apt=$(sudo apt-get install -y tmux vim stress lynx git curl wget)
 download_speedtest_cli=$(curl -s https://packagecloud.io/install/repositories/ookla/speedtest-cli/script.deb.sh | sudo bash)
 install_speedtest_cli=$(sudo apt-get install -y speedtest)
-)
+
 
 echo "installing from apt: tmux, vim, stress, lunx, git, curl, wget"
 echo "$install_from_apt"
